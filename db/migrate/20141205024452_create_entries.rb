6ae7012760db9b1288097b35b800675c64de0923
Santita Dwi Putri <santitadwiputri@Santitas-MacBook-Pro.local>
2014-12-04 21:50:34 -0500

    generate scaffold for entry and location

--- db/migrate/20141205024452_create_entries.rb
+++ db/migrate/20141205024452_create_entries.rb
@@ -1,9 +1,11 @@
-class CreateEntry < ActiveRecord::Migration
+class CreateEntries < ActiveRecord::Migration
   def change
     create_table :entries do |t|
       t.string :title
       t.string :summary
       t.string :url
+
+      t.timestamps
     end
   end
 end
--- db/migrate/20141205024801_create_locations.rb
+++ db/migrate/20141205024801_create_locations.rb
@@ -1,10 +1,12 @@
-class CreateLocation < ActiveRecord::Migration
+class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
       t.string :city
       t.string :state
       t.string :country
-      t.integer :entry_id
+      t.references :entry, index: true
+
+      t.timestamps
     end
   end
 end
